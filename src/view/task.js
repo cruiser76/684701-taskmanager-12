8b66732278b49d8ba2beeee6f3b1e91e97e22059
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-08-30 17:38:14 +0600

    Корректировки

--- src/view/task.js
+++ src/view/task.js
@@ -3,7 +3,6 @@ import Abstract from './abstract.js';
 
 const createTaskTemplate = (task) => {
   const {color, description, dueDate, repeatingDays, isArchive, isFavorite} = task;
-
   const date = formatTaskDueDate(dueDate);
 
   const deadlineClassName = isTaskExpired(dueDate)
